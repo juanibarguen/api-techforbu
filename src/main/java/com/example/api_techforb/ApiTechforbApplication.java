f5175da833c6b7e1bc2cfed43520fe704da4f72c
Juan Cruz Ibarguen <juanibarguen159@gmail.com>
2024-10-27 18:01:21 +0100

    tests

--- src/main/java/com/example/api_techforb/ApiTechforbApplication.java
+++ src/main/java/com/example/api_techforb/ApiTechforbApplication.java
@@ -5,9 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class ApiTechforbApplication {
-
-	public static void main(String[] args) {
-		SpringApplication.run(ApiTechforbApplication.class, args);
-	}
-
+    public static void main(String[] args) {
+        SpringApplication.run(ApiTechforbApplication.class, args);
+    }
 }
--- src/test/java/com/example/api_techforb/ApiTechforbApplicationTests.java
+++ src/test/java/com/example/api_techforb/ApiTechforbApplicationTests.java
@@ -2,12 +2,12 @@ package com.example.api_techforb;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
 
 @SpringBootTest
+@ActiveProfiles("test") // Asegúrate de tener este perfil de pruebas configurado
 class ApiTechforbApplicationTests {
-
-	@Test
-	void contextLoads() {
-	}
-
+    @Test
+    void contextLoads() {
+    }
 }
